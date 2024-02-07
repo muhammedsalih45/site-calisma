61f636973e326b5c7573a681e95aa865cfa6bf69
muhammedsalih45 <muhammedsalihyumuk@gmail.com>
2024-02-07 10:36:40 +0300

    search kısmının tüm fonksiyonları bitti

---
+++ site-calisma1-Main/filtrele.js
@@ -0,0 +1,287 @@
+var games = [
+  {
+    Yıl: "2013",
+    "Oyun Adı": "Grand Theft Auto V",
+    Platformlar: "PlayStation 3, PlayStation 4, Xbox 360, Xbox One, Windows",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2013",
+    "Oyun Adı": "The Last of Us",
+    Platformlar: "PlayStation 3, PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2013",
+    "Oyun Adı": "BioShock Infinite",
+    Platformlar: "PlayStation 3, Xbox 360, Windows",
+    Tür: "Birinci şahıs nişancı",
+  },
+  {
+    Yıl: "2014",
+    "Oyun Adı": "Dragon Age: Inquisition",
+    Platformlar: "PlayStation 3, PlayStation 4, Xbox 360, Xbox One, Windows",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2014",
+    "Oyun Adı": "Middle-earth: Shadow of Mordor",
+    Platformlar: "PlayStation 3, PlayStation 4, Xbox 360, Xbox One, Windows",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2015",
+    "Oyun Adı": "The Witcher 3: Wild Hunt",
+    Platformlar: "PlayStation 4, Xbox One, Windows",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2015",
+    "Oyun Adı": "Metal Gear Solid V: The Phantom Pain",
+    Platformlar: "PlayStation 3, PlayStation 4, Xbox 360, Xbox One, Windows",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2015",
+    "Oyun Adı": "Fallout 4",
+    Platformlar: "PlayStation 4, Xbox One, Windows",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2016",
+    "Oyun Adı": "Uncharted 4: A Thief's End",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2016",
+    "Oyun Adı": "Overwatch",
+    Platformlar: "PlayStation 4, Xbox One, Windows",
+    Tür: "Kahraman nişancı oyunu",
+  },
+  {
+    Yıl: "2017",
+    "Oyun Adı": "The Legend of Zelda: Breath of the Wild",
+    Platformlar: "Nintendo Switch, Wii U",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2017",
+    "Oyun Adı": "Persona 5",
+    Platformlar: "PlayStation 3, PlayStation 4",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2017",
+    "Oyun Adı": "Horizon Zero Dawn",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2018",
+    "Oyun Adı": "God of War",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2018",
+    "Oyun Adı": "Red Dead Redemption 2",
+    Platformlar: "PlayStation 4, Xbox One",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2018",
+    "Oyun Adı": "Spider-Man",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2019",
+    "Oyun Adı": "Sekiro: Shadows Die Twice",
+    Platformlar: "PlayStation 4, Xbox One, Windows",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2019",
+    "Oyun Adı": "Resident Evil 2",
+    Platformlar: "PlayStation 4, Xbox One, Windows",
+    Tür: "Hayatta kalma korku",
+  },
+  {
+    Yıl: "2019",
+    "Oyun Adı": "Death Stranding",
+    Platformlar: "PlayStation 4, Windows",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2020",
+    "Oyun Adı": "The Last of Us Part II",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2020",
+    "Oyun Adı": "Ghost of Tsushima",
+    Platformlar: "PlayStation 4",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2020",
+    "Oyun Adı": "Animal Crossing: New Horizons",
+    Platformlar: "Nintendo Switch",
+    Tür: "Yaşam simülasyonu",
+  },
+  {
+    Yıl: "2021",
+    "Oyun Adı": "Hades",
+    Platformlar: "Nintendo Switch, Windows",
+    Tür: "Roguelike",
+  },
+  {
+    Yıl: "2021",
+    "Oyun Adı": "Ratchet & Clank: Rift Apart",
+    Platformlar: "PlayStation 5",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2021",
+    "Oyun Adı": "Resident Evil Village",
+    Platformlar:
+      "PlayStation 4, PlayStation 5, Xbox One, Xbox Series X/S, Windows",
+    Tür: "Hayatta kalma korku",
+  },
+  {
+    Yıl: "2022",
+    "Oyun Adı": "Elden Ring",
+    Platformlar:
+      "PlayStation 4, PlayStation 5, Xbox One, Xbox Series X/S, Windows",
+    Tür: "Aksiyon rol yapma oyunu",
+  },
+  {
+    Yıl: "2022",
+    "Oyun Adı": "God of War Ragnarök",
+    Platformlar: "PlayStation 4, PlayStation 5",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2022",
+    "Oyun Adı": "Horizon Forbidden West",
+    Platformlar: "PlayStation 4, PlayStation 5",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2023",
+    "Oyun Adı": "The Legend of Zelda: Tears of the Kingdom",
+    Platformlar: "Nintendo Switch",
+    Tür: "Aksiyon-macera",
+  },
+  {
+    Yıl: "2023",
+    "Oyun Adı": "Starfield",
+    Platformlar: "Windows, Xbox Series X/S",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2023",
+    "Oyun Adı": "Final Fantasy XVI",
+    Platformlar: "PlayStation 5",
+    Tür: "Rol yapma oyunu",
+  },
+  {
+    Yıl: "2023",
+    "Oyun Adı": "Spider-Man 2",
+    Platformlar: "PlayStation 5",
+    Tür: "Aksiyon-macera",
+  },
+];
+
+const gamesearchBox = document.getElementById("searchInput");
+const searchList = document.getElementById("gameList");
+document.addEventListener("DOMContentLoaded", function () {
+  // Sayfa yüklendiğinde gamelisti gizle
+  // Sayfa üzerinde bir yere tıklandığında gamelisti gizle
+  document.addEventListener("click", function (event) {
+    if (
+      !event.target.closest("#searchInput") &&
+      !event.target.closest("#gameList")
+    ) {
+    }
+  });
+});
+
+// Arama ve Filtreleme Fonksiyonu
+function filterGames() {
+  var searchTerm = document.getElementById("searchInput").value.toLowerCase();
+  var filteredGames = games.filter(function (game) {
+    return game["Oyun Adı"].toLowerCase().includes(searchTerm);
+  });
+
+  displayGames(filteredGames);
+}
+
+function displayGames(games) {
+  searchList.innerHtML = "";
+  for (let idx = 0; idx < games.length; idx++) {
+    let gameListItem = document.createElement("div");
+    gameListItem.dataset.id = games[idx]["Oyun Adı"];
+    gameListeItem.classList.add("gameList-item");
+    if (games[idx] == 0) {
+      gameListItem.innerHTML = `
+            <div class="">
+            `;
+    }
+  }
+}
+
+function displayGames(games) {
+  var gameListElement = document.getElementById("gameList");
+  gameListElement.innerHTML = "";
+
+  games.forEach(function (game) {
+    var listItem = document.createElement("li");
+    listItem.textContent = game["Oyun Adı"];
+    listItem.addEventListener("click", function () {
+      showGameDetails(game); // Buradaki değişiklik
+    });
+    gameListElement.appendChild(listItem);
+  });
+}
+
+function showGameDetails(game) {
+  var gameDetailsElement = document.getElementById("gameDetails");
+  gameDetailsElement.innerHTML = "";
+
+  var details = document.createElement("div");
+  details.innerHTML = `
+        <h2>${game["Oyun Adı"]}</h2>
+        <p>Yıl: ${game["Yıl"]}</p>
+        <p>Platformlar: ${game["Platformlar"]}</p>
+        <p>Tür: ${game["Tür"]}</p>
+    `;
+
+  gameDetailsElement.appendChild(details);
+}
+document.getElementById("searchInput").addEventListener("input", filterGames);
+window.addEventListener("click", (event) => {
+  if (event.target.className != "form-control search") {
+    searchList.classList.add("hide-gameList");
+  }
+});
+
+const selectElement = document.getElementById("searchInput");
+
+selectElement.addEventListener("input", (event) => {
+  if (event.target.value.length > 0) {
+    document.getElementById("gameList").classList.remove("displayNone");
+  } else {
+    document.getElementById("gameList").classList.add("displayNone");
+  }
+});
+
+const gameElement = document.getElementById("gameList");
+
+gameElement.addEventListener("click", (event) => {
+  if (gameElement.click) {
+    document.getElementById("gameList").classList.add("displayNone");
+  }
+});
